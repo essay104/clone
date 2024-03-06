ecf0bc38ac0136f0866a794c0e2f0d1a3e006ed5
essay104 <156299961+essay104@users.noreply.github.com>
2024-03-07 01:49:50 +0900

    240307

--- 복습자료/script.js
+++ 복습자료/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menu = document.querySelector("#study");
 const btn = document.querySelector(".move");
 const option = document.querySelectorAll("option");
@@ -8,14 +7,3 @@ movingPage = () => {
 };
 
 btn.addEventListener("click", movingPage);
-=======
-const menu = document.querySelector("#study");
-const btn = document.querySelector(".move");
-const option = document.querySelectorAll("option");
-
-movingPage = () => {
-  window.location.href("index2.html");
-};
-
-btn.addEventListener("click", movingPage);
->>>>>>> 2e043159f842958136351ea2fe8e8c0cb004ab3b

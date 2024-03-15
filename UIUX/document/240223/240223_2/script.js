f1e5fff01878f18166828113cc95ea25ba2808e6
essay104 <aleixs104@naver.com>
2024-03-15 14:17:32 +0900

    240315

--- UIUX/document/240223/240223_2/script.js
+++ UIUX/document/240223/240223_2/script.js
@@ -1,6 +1,11 @@
 const cButtons = document.querySelector(".check");
 const dButtons = document.querySelector(".delete");
+const products = document.querySelector("#products");
 
 dButtons.addEventListener("click", function () {
-  this.parentNode.remove();
+  products.classList.add("none");
+});
+
+cButtons.addEventListener("click", () => {
+  products.classList.remove("none");
 });

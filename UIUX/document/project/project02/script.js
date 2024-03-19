7fee78a5f143ef670cc7b0ffaf6ce94015009b05
essay104 <156299961+essay104@users.noreply.github.com>
2024-03-19 23:30:04 +0900

    240319

--- UIUX/document/project/project02/script.js
+++ UIUX/document/project/project02/script.js
@@ -1,7 +1,28 @@
 // 워밍업!!
 
 const text = document.querySelector('input[type="text"]');
+const form = document.querySelector("form");
 
 text.addEventListener("click", () => {
   text.placeholder = "";
 });
+
+// 1단계 todo 이벤트 만들기
+
+console.log(form);
+
+const createItems = (e) => {
+  e.preventDefault();
+
+  const createElement = (listId, item) => {
+    const list = document.querySelector(`#${listId}`);
+    const item = document.createElement("div");
+  };
+
+  const createTodo = (e) => {
+    e.preventDefault();
+    
+  };
+};
+
+form.addEventListener("submit", createItems);
